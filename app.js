abd6ee6de300bd64d8c2fe0a0836967953be7f34
TracyOakley <tloak12@gmail.com>
2022-09-22 19:00:35 +0000

    Improving site with functions

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
-let userInfo = prompt('Where are you now?');
+//invoking
+greetUser();
 
-if (userInfo == null || userInfo == "" ){
-    alert("That's not cool, you are suppose to put your location.");
-    userInfo = prompt("Where are you at now?");
+let userInfo = askUserforInfo();
+
+let infoNotBlank = checkValidInput(userInfo);
+//this iswhere I am doing the stretch goal.
+
+judgeUserLocation(infoNotBlank);
+
+
+
+
+//declaring
+function greetUser(){
+    alert("Welcome to Travel Destination!");
 }
 
-console.log("Beach is the best place.")
-
-if(userInfo == "Beach"){
-    alert("Nice!");
-    document.write("Enjoy that Cool Breeze!");
-} else {
-    alert(userInfo + "  is cool I guess.");
-    document.write("Being at the Beach would be better..");
+function askUserforInfo (){
+    let userInfo = prompt('Where are you now?');
+    console.log(userInfo);
+    return userInfo;
 }
+
+function checkValidInput(inputToCheck){
+    if (inputToCheck == null || inputToCheck == "" ){
+        alert("That's not cool, you are suppose to put your location."); 
+        let newUserInfo = checkValidInput(askUserforInfo());
+        return newUserInfo;
+    } else {
+        return inputToCheck;
+    }
+}
+
+function judgeUserLocation(userInfo){
+
+    console.log("Beach is the best place.");
+
+    if(userInfo == "Beach" || userInfo == "beach"){
+        alert("Nice!");
+
+        document.write("Enjoy that Cool Breeze!");
+    } else {
+        alert(userInfo + "  is cool I guess.");
+
+        document.write("Being at the Beach would be better..");
+    }
+}
+
 
